8c99bd004b7cd44b37e89d1b06beeac824339438
avijit <avijitroy2944@gmail.com>
2023-02-11 12:52:05 +0530

    commit1

--- server.js
+++ server.js
@@ -16,11 +16,13 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
-app.use(cors({
-    origin: process.env.FRONTEND_URL,
-    credentials: true,
-    methods: ["GET","POST","PUT","DELETE"],
-}));
+app.use(
+    cors({
+      origin: process.env.FRONTEND_URL,
+      credentials: true,
+      methods: ["GET", "POST", "PUT", "DELETE"],
+    })
+  );
 
 connectDB();
 cloudinary.v2.config({
